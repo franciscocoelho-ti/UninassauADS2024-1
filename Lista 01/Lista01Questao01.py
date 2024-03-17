17c1ae6b9e7fdc6480050b8d50257505245fe9b7
Francisco Coelho <franciscocoelho.ti@gmail.com>
2024-03-17 19:35:56 -0300

    Adicionando Novos Scripts

--- Lista 01/Lista01Questao01.py
+++ Lista 01/Lista01Questao01.py
@@ -1,2 +1,5 @@
-# Escreva um algoritmo em Python que receba de um usuário a altura de um determinado andar de um prédio, e quantos andares o prédio possui. Ao final mostre a altura total do prédio.
+# Escreva um algoritmo em Python que receba de um usuário a altura de um determinado
+# andar de um prédio, e quantos andares o prédio possui. Ao final mostre a altura
+# total do prédio.
+
 
---
+++ Lista 01/Lista01Questao02.py
@@ -0,0 +1,4 @@
+# Simulando uma competição esportiva, um determinado atleta tem sua nota gerada a partir
+# da média das notas atribuída pelos três jurados. Crie um algoritmo em Python que receba
+# as notas dos jurados e mostre e nota final do atleta.
+
